1f2092ac81e7a0bb726a31efdd01fe2fff8f4034
davidgljay <davidgljay@gmail.com>
2011-06-11 16:39:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osra::Application.config.secret_token = '535bea345eb532543ad20709110f0cf6c5adf0676852b33b25baa7238b49e0473da7a1dc953726a3353dac1b4798e4ab4040786ac822fd9303e304acd0207b8b'

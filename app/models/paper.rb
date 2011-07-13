df1b8d253a00dc62828b895c7897281c165fe2d1
davidgljay <davidgljay@gmail.com>
2011-07-13 00:37:35 -0400

    Finished with XML sync

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,5 @@
 class Paper < ActiveRecord::Base
+require 'rexml/document'
 
 has_many :authorships, :foreign_key => "paper_id",
                            :dependent => :destroy
@@ -8,4 +9,19 @@ has_many :authors, :through => :authorships, :source => :author
    validates :pubmed_id, :presence => true,
                          :length => { :is => 8 },
                          :uniqueness => true
+
+def lookup_info
+  url = 'http://www.ncbi.nlm.nih.gov/pubmed/' + self.pubmed_id + '?report=xml'
+  dirty_xml = Net::HTTP.get_response(URI.parse(url)).body
+  clean_xml = CGI::unescapeHTML(dirty_xml)
+  data = REXML::Document.new(clean_xml)
+  article = data.root.elements["PubmedArticle"].elements["MedlineCitation"].elements["Article"]
+  self.title = article.elements["ArticleTitle"].text
+  self.journal = article.elements["Journal"].elements["ISOAbbreviation"].text
+  unless article.elements["Abstract"].elements["AbstractText"].nil?
+    self.abstract = article.elements["Abstract"].elements["AbstractText"].text
+  end
+  self.save
+end
+
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,16 +3,10 @@ require 'spec_helper'
 describe Paper do
  
   before(:each) do
-    @attr = {
-             :title => "Sample Paper",
-             :pubmed_id => "12345678",
-             :journal => "XYZ Journal",
-             :abstract => "abstract",
-             :summary => "summary"
-             }
+    @attr = {:pubmed_id => "12345678"}
   end         
 
-  it "should create a new instance given valid attributes" do
+  it "should create a  new instance given valid attributes" do
     Paper.create!(@attr)
   end
 
@@ -22,19 +16,32 @@ describe Paper do
   end
 
   it "should require the pubmed id to be 8 characters" do
-    short_pubmed_paper = Paper.new(@attr.merge(:pubmed_id => "1"))
+    short_pubmed_paper = Paper.new(@attr.merge(:pubmed_id => "1234567"))
+    short_pubmed_paper2 = Paper.new(@attr.merge(:pubmed_id => "123456789"))
     short_pubmed_paper.should_not be_valid
+    short_pubmed_paper2.should_not be_valid
   end
 
   describe "author associations" do
      before(:each) do
         @paper = Paper.create!(@attr)
         @author = Author.create({:firstname => "David", "lastname" => "Jay"})
-        @author.authorships.create(@paper)
+        @paper.authorships.create(:author_id => @author.id)
      end
 
      it "should exist" do
-        @paper.authors.should include(@author)
+        @paper.authors.all.should include(@author)
+     end
+  end
+
+ describe "paper data population" do
+     before(:each) do
+          @attr = {:pubmed_id => "18276894"}
+          @paper = Paper.create!(@attr)
+     end
+
+     it "should bring in data from pubmed" do
+         @paper.title.should include("The critical importance of retrieval for learning.")
      end
   end
 end

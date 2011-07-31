c81809b5fc4aa4af07bcd197061d58e6924fc855
davidgljay <davidgljay@gmail.com>
2011-07-31 04:10:29 -0700

    Commiting removed files

--- db/migrate/20110719142826_add_methods_to_assertion.rb
+++
@@ -1,9 +0,0 @@
-class AddMethodToAssertion < ActiveRecord::Migration
-  def self.up
-    add_column :assertions, :method, :text
-  end
-
-  def self.down
-    remove_column :assertions, :method
-  end
-end

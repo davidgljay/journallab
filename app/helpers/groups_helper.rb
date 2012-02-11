7d28c9cbd576dff11873bb5d3a07e7acc380ab73
davidgljay <davidgljay@gmail.com>
2012-02-11 14:39:36 -0800

    Added helpers to model b/c heroku console is being odd.

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,6 +2,8 @@ require 'fastercsv'
 
 module GroupsHelper
 
+#Moving these into a model until I can figure out why they don't work on heroku...
+
 #Takes a 2D array
 def export_data(array, name = "data")
   FasterCSV.open("/public/data/" + name + "_" + Time.now.strftime("%m_%d_%Y_%H:%M:%S") + ".csv", "w") do |csv|
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -13,4 +13,33 @@ validates_uniqueness_of :user_id, :scope => [:paper_id]
     count ||= 0
   end
 
+#These live here temporarily until I can figure out why helpers don't work in the heroku console
+
+#Takes a 2D array
+def export_data(array, name = "data")
+  FasterCSV.open("/public/data/" + name + "_" + Time.now.strftime("%m_%d_%Y_%H:%M:%S") + ".csv", "w") do |csv|
+
+    csv << array
+  end
+end
+
+#Takes an array, returns a frequency array by day
+
+def graph_by_day(array)
+    array.sort!{|x,y| x.created_at <=> y.created_at}
+    finish = array.last.created_at + 1.day
+    start = array.first.created_at -1.day
+    days = []
+    day = start.midnight
+    while day < finish.midnight
+       days << day
+       day += 1.day
+    end
+    formatted_date = days.map{|day| day.strftime("%D")}
+    values = days.map{|day| array.select{|object| object.created_at > day && object.created_at < day + 1.day}.count}
+    graph = [formatted_date,values]
+end
+
+
+
 end

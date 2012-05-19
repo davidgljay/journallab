fb80b6fe3f0b2bdcd9a1e502c9b9a0817b6f9d8c
David Jay <davidgljay@gmail.com>
2012-05-19 16:50:19 -0700

    Connected dragonfly directly to S3.

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -4,7 +4,7 @@ app = Dragonfly[:images]
 app.configure_with(:imagemagick)
 app.configure_with(:rails)
 #app.configure_with(:heroku, 'j.lab-images') if Rails.env.production?
-  c.datastore = Dragonfly::DataStorage::S3DataStore.new(
+app.datastore = Dragonfly::DataStorage::S3DataStore.new(
     :bucket_name => 'j.lab-images',
     :access_key_id => 'AKIAJ6SR4UCCJDDU67TA',
     :secret_access_key => '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'

e6a50f77abbad0a0b2bdb9b65b75ee63f0e70cfd
David Jay <davidgljay@gmail.com>
2012-11-17 17:02:22 -0800

    Fixing error where feed does not display.

--- spec/requests/signup_spec.rb
+++ spec/requests/signup_spec.rb
@@ -5,25 +5,25 @@ describe "Signup" do
   it "should create a new user account" do
     visit root_path
     click_link 'Sign Up'
-    fill_in 'user_firstname' "Andrea"
-    fill_in 'user_lastname' "Torres"
-    fill_in 'user_email' "andreatorres@gmail.com"
-    fill_in 'user_password' "dogsarethebest"
-    fill_in 'user_password_confirmation' "dogsarethebest"
+    fill_in 'user_firstname', :with => "Andrea"
+    fill_in 'user_lastname', :with => "Torres"
+    fill_in 'user_email', :with => "andreatorres@gmail.com"
+    fill_in 'user_password', :with => "dogsarethebest"
+    fill_in 'user_password_confirmation', :with => "dogsarethebest"
     click_button 'Sign up'
     page.should have_content('Sign Out')
     assert !ActionMailer::Base.deliveries.empty?
     email = ActionMailer::Base.deliveries.last
-    email.subject.should_include 'Confirmation'
+    email.subject.include?('Confirmation').should be_true
   end
 
   it "should not create a user account if there is an error" do
     visit root_path
     click_link 'Sign Up'
-    fill_in 'user_firstname' "Andrea"
-    fill_in 'user_lastname' "Torres"
-    fill_in 'user_password' "dogsarethebest"
-    fill_in 'user_password_confirmation' "dogsarethebest"
+    fill_in 'user_firstname', :with => "Andrea"
+    fill_in 'user_lastname', :with => "Torres"
+    fill_in 'user_password', :with => "dogsarethebest"
+    fill_in 'user_password_confirmation', :with => "dogsarethebest"
     click_button 'Sign up'
     page.should have_content('Email is blank')
   end

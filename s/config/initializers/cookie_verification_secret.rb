8cb62588dd84fcb363ef348d5465f8b246792cd4
David Jay <davidgljay@gmail.com>
2013-01-31 15:21:45 -0800

    Cleaning up messy directory.

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '45ef40271249c0bd3f6a072ed561c6dd5feb8e168b9a32135eaf415f5f0d863f92fb17833d575798dd8ba7490144efccd61d30de094168a6b44ff1d48344d0f4';

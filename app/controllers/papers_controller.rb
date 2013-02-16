8f0b1af847b12e0b30f2c342b9621a81ed41461e
David Jay <davidgljay@gmail.com>
2013-02-15 16:53:49 -0800

    Updated conf email language.

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -23,7 +23,7 @@ class PapersController < ApplicationController
     @heatmap = @paper.heatmap
     @heatmap_overview = @paper.heatmap_overview
     @reaction_map = @paper.reaction_map
-    @interest = @paper.set_interest
+    #@interest = @paper.set_interest
     #@blogs = @paper.check_blogs
     if signed_in?
       @groups = current_user.groups

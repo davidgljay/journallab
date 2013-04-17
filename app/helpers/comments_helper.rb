17929cc69c2130a370d6759c400fd9df0ad60688
David Jay <davidgljay@gmail.com>
2013-04-16 23:17:12 -0700

    Added Procfile to define app's process-formation on Heroku/Cedar

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,4 +4,8 @@ def boxtext(reply_to)
 	reply_to ? 'Leave a reply.' : 'Please explain your reaction (optional).'
 end
 
+def addtl_comments(owner, comment)
+  (owner.comments + comment.owner.comments + comment.comments).uniq.count-1
+end
+
 end

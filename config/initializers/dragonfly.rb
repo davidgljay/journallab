b03595f95abfd17e3c3f3c81cff5fdb47f2dacea
David Jay <davidgljay@gmail.com>
2014-03-09 14:37:21 -0400

    Fixing issue with Amazon S3

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -6,8 +6,8 @@ app.configure_with(:rails)
 #app.configure_with(:heroku, 'j.lab-images') if Rails.env.production?
 app.datastore = Dragonfly::DataStorage::S3DataStore.new(
     :bucket_name => 'j.lab-images',
-    :access_key_id => 'AKIAJ6SR4UCCJDDU67TA',
-    :secret_access_key => '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'
+    :access_key_id => ENV['S3_ACCESS_KEY'],
+    :secret_access_key => ENV['S3_ACCESS_SECRET']
   )
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
